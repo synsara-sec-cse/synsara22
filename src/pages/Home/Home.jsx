1758a131bd93a929932a20523258586c580574c6
Riduvarshini-S-R <61472369+Riduvarshini-S-R@users.noreply.github.com>
2022-09-19 15:14:25 +0530

    Add files via upload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,33 +8,13 @@ import Aboutdept from "./Aboutdept";
 import Nav from "../../components/Nav/Nav";
 import Footer from "../../components/Footer/Footer";
 import Bgweb from "./bgweb.png";
-import homepage from"./homepagefinal.gif";
+
 const Home = () => {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(!loading);
-    setTimeout(() => {
-      setLoading(loading);
-    }, 2950);
-  }, []);
+ 
 
   return(
     <>
-     { loading?(
-      // <div className="loader-container">
-      //   <div className="spinner"></div>
-      <div class="flex justify-center items-center" >
-      {/* <img
-        src="homepage.gif"
-        class="max-w-sm h-auto shadow-lg"
-        alt=""
-      /> */}
-      {/* <img src={homepage} class="max-w-full h-full"/> */}
-      <div className="loader-container">
-        <img src={homepage} class="max-w-full h-full"/>
-      </div>
-      </div>
-):(
+     
   
   
     <div style={{backgroundImage: `url(${Bgweb})`,backgroundSize: 'contain',}}>
@@ -45,16 +25,17 @@ const Home = () => {
       <br></br>
       <br/><br/>
       <br/>
+      <Aboutsynsara/>
       <Aboutinst/>
       <Aboutdept/>
       
-      <Aboutsynsara/>
+      
       <br/>
       <Footer/>
       </div>
       
       
-)};
+
   </>   
   );
 }
